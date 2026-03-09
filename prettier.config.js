d929013d4d19dc0d68afd2d210c4c72302fd54c7
Nico Kaiser <nico@kaiser.me>
2026-03-09 08:30:15 +0100

    update

--- prettier.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('prettier').Config} */
-module.exports = {
-  plugins: ["prettier-plugin-css-order", "prettier-plugin-organize-attributes"],
-  printWidth: 1000,
-  tabWidth: 2,
-};
